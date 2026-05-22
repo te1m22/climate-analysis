041b3b1bb2a2eaa788a9b1dfac746e9cc18e4564
te1m22 <T.Emmanuel@soton.ac.uk>
2026-05-22 12:29:51 +0100

    Update temp_conversion.py
    updated functions to have docstrings

--- temp_conversion.py
+++ temp_conversion.py
@@ -1,10 +1,20 @@
 # Todo: Code is a bit unclear
 
-def Fc(x):
-    Y = (x - 32) * (5 / 9)
-    return Y
+def convert_fahr_to_celsius(fahr):
+    """
+    Given a temperature in Farenheit, converts to Celsius.
+    :param fahr: The temperature in fahrenheit
+    :raises ValueError: If the temperature is below zero
+    :return: The temperature in celsius
+    """
+    celsius = (fahr - 32) * (5 / 9)
+    if celsius < 273.15
+        raise ValueError(
+            f"Trying to convert impossible temperature: {fahr}F"
+        )
+    return celsius
 
-def FK(x):
-    y = Fc(x)
-    z = y + 273.15
-    return z
+def convert_fahr_to_kelvin(fahr):
+    celsius = convert_fahr_to_celsius(fahr)
+    kelvin = celsius + 273.15
+    return kelvin
